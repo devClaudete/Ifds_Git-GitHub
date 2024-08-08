76a3aff4bbabc5a8c05eec7e943a4c007a1f4bf0
devclaudete <claudete.oa@ifsp.edu.br>
2024-08-08 13:02:40 -0300

    subindo todos os arquivos

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 //criando uma variável
 let mensagem1="Bom dia, estamos aprendendo";
 let mensagem2="JavaScript";
+let mensagem3="Uau";
 
 //exibindo o resultado
 console.log(mensagem1);
 console.log(mensagem2);
+console.log(mensagem3);
